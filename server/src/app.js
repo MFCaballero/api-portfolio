bad60cc7f071d161773b06e5a527f3adc8022e06
florencia caballero <florcha12.caballero@gmail.com>
2021-07-24 17:35:37 -0300

    adding deployed client origin

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ require("./auth/passportGoogleSSO");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(morgan("dev"));
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://60fc7775afc7e680446b9830--xenodochial-clarke-bd4d3f.netlify.app/", credentials: true }));
 app.use(express.json());
 
 app.set("trust proxy", 1);
